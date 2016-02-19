1ea3e9772ffa670b680f6df0e4077fede23c4c2b
Engin Kurutepe <engin@kurutepe.com>
2016-02-19 14:02:22 +0100

    Initial Commit

---
+++ ASDKTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ASDKTest
+//
+//  Created by Engin Kurutepe on 19/02/16.
+//  Copyright © 2016 Engin Kurutepe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ASDKTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ASDKTest
+//
+//  Created by Engin Kurutepe on 19/02/16.
+//  Copyright © 2016 Engin Kurutepe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
